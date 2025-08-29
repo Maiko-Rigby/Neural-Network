bba6e3b57ea23e5605d9c7d2fd3ade50960652b1
Maiko-Rigby <michaelrigby28@gmail.com>
2025-08-29 23:49:39 +0100

    Started Neural Network Class

--- Neural-Network.py
+++ Neural-Network.py
@@ -4,3 +4,10 @@ from sklearn.model_selection import train_test_split
 import tensorflow as ts
 from tensorflow import keras
 
+class NeuralNetwork:
+    def __init__(self):
+        # Initialise a neural network
+
+        self.lr = learning_rate
+
+        self.W1 = np.random.randint(input_size, hidden_size)
