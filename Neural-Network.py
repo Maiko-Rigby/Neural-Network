3d20451cdb8016c0a8701cbaab7b399fdd5a0519
Maiko-Rigby <michaelrigby28@gmail.com>
2025-08-31 15:30:23 +0100

    Completed Neural Network

--- Neural-Network.py
+++ Neural-Network.py
@@ -5,9 +5,107 @@ import tensorflow as ts
 from tensorflow import keras
 
 class NeuralNetwork:
-    def __init__(self):
+    def __init__(self, learning_rate, input_size, hidden_size, output_size):
         # Initialise a neural network
 
         self.lr = learning_rate
 
-        self.W1 = np.random.randint(input_size, hidden_size)
+
+        self.W1 = np.random.randint(input_size, hidden_size) * 0.01
+        self.B1 = np.zeros((1, hidden_size))
+        self.W2 = np.random.randint(hidden_size, output_size ) * 0.01
+        self.B2 = np.zeros((1, output_size))
+
+    def sigmoid(self,z):
+        # Takes any number and squishes it into a value between 0 and 1
+
+        z = np.clip(z, -500, 500) # Clipping the Z value prevents overflow
+        return 1 / (1 + np.exp(-1))
+    
+    def sigmoid_derivative(self,z):
+        s = self.sigmoid(z)
+        return s * (1-s)
+    
+    def softmax(self,z):
+        # softmax allows for multi-class classification
+        exp_z = np.exp(z - np.max(z, axis=1, keepdims=True))
+        return exp_z / np.sum(exp_z, axis=1, keepdims=True)
+    
+    def forward(self, inputData):
+        # Forward propagation, input data moves through each layer of the neural network
+
+        # Layer 1   -   Following Z = WX + B 
+        self.Z1 = np.dot(inputData, self.W1) + self.B1
+        self.A1 = self.sigmoid(self.Z1)
+
+        # Layer 2 (output)  -  Using softmax for multi-class classification
+        self.Z2 = np.dot(self.A1, self.W2) + self.B2
+        self.A2 = self.softmax(self.Z2)
+
+        return self.A2
+    
+    def backward(self, inputData, trueLabels, output):
+
+        m = inputData.shape[0]
+
+        # Calculate the gradients for the output layer
+        # negative -> prediction too low, increase output. vice versa
+        # This calculates how much each connection contributed
+
+        dZ2 = output - trueLabels
+        dW2 = (1/m) * np.dot(self.A1.T, dZ2)
+        db2 = (1/m) * np.sum(dZ2, axis=0, keepdims=True) # Each gradient tells us how to adjust the weight from that neuron to the output
+
+        # Calculate the gradients for the hidden layer
+
+        dZ1 = np.dot(dZ2, self.W2.T) * self.sigmoid_derivative(self.Z1)
+        dW1 = (1/m) * np.dot(self.X.T, dZ2)
+        db1 = (1/m) * np.sum(dZ1, axis=0, keepdims=True)
+
+        # Update the weights and biases
+        self.W2 -= self.lr * dW2
+        self.b2 -= self.lr * db2
+        self.W1 -= self.lr * dW1
+        self.b1 -= self.lr * db1
+
+    def train(self, inputData, trueLabels, epochs = 1000):
+        
+        losses = []
+
+        for epoch in range(epochs):
+            # Perform a forward propagation
+
+            output = self.forward(inputData)
+
+            # Calculate loss
+
+            loss = self.compute_loss(trueLabels, output)
+            losses.append(loss)
+
+            # Perform a backwards propagation
+
+            self.backward(inputData, trueLabels, output)
+
+            if epoch % 100 == 0:
+                print(f'Epoch {epoch}, Loss: {loss:.4f}')
+
+        
+        return losses
+
+    def compute_loss(self, trueLabels, output):
+        # Computing binary cross-entropy loss
+            
+        m = trueLabels.shape[0]
+
+        output = np.clip(output, 1e-7, 1-1e-7)
+        loss = -1/m * np.sum(trueLabels * np.log(output) + (1 - trueLabels) * np.log(1 - output))
+            
+        return loss
+    
+    def predict(self, inputData):
+        # Make the predictions using the input data
+        output = self.forward(inputData)
+        return (output > 0.5).astype(int)
+
+
+        
